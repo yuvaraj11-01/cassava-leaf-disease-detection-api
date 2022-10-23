dcd12f1be19c5fceaa660c49bd778614ad4c7b2f
Leaf-Game-Dev <yuvaraj1101m@gmail.com>
2022-10-23 14:00:46 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.use("/",(req,res)=>{
+    res.json({message:"Hello"});
+});
+
+app.listen(port,()=>{
+    console.log(`Starting server on port ${port}`);
+
+});
